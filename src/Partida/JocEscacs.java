be601457f3da2224c237e781808408faea5601e6
MarcRogerUDG <62808807+MarcRogerUDG@users.noreply.github.com>
2020-05-02 16:19:51 +0200

    Commit prova

--- src/Partida/JocEscacs.java
+++ src/Partida/JocEscacs.java
@@ -1,4 +1,25 @@
 package Partida;
 
-public class JocEscacs {
+import javafx.application.Application;
+
+public abstract class JocEscacs {
+
+    public static void main(String[] args){
+        try {
+            if (args.length == 2) {
+                int files = Integer.parseInt(args[0]);
+                int columnes = Integer.parseInt(args[1]);
+                PartidaText.juga(new Partida(files,columnes));
+                return;
+            }
+            else if (args.length == 3 && args[2].equals("-g")) {
+                Application.launch(PartidaGrafica.class, args);
+                return;
+            }
+        }
+        catch (Exception e) {
+            System.out.println("S'ha produït una excepció.");
+            e.printStackTrace();
+        }
+    }
 }

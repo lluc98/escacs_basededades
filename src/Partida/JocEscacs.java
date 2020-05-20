7f3f17dcb4d2f6432f94f489ac49f2f0e269b3fa
MarcRogerUDG <62808807+MarcRogerUDG@users.noreply.github.com>
2020-05-20 15:22:28 +0200

    [MODIFIED] JocEscacs.java fet, PartidaText.java començat i bàsic fet, PartidaGrafica.java bastant avançada la primera part.

--- src/Partida/JocEscacs.java
+++ src/Partida/JocEscacs.java
@@ -5,15 +5,25 @@ import javafx.application.Application;
 public abstract class JocEscacs {
 
     public static void main(String[] args){
+        /**
+         * 1.-Preguntar si es vol començar una partida o carregar-ne una
+         * 2.1-Si es vol començar una partida, preguntar pel fitxer de regles (es pregunta aqui perquè si ho preguntessim després de preguntar si es vol que
+         * sigui de text o gràfic, repetiriem codi)
+         * 2.1.1.-Preguntar si es vol crear una partida de text o gràfica
+         * 2.1.1.1.- Si es vol partida de text, crear un "partidaText" on li passem el nom del fitxer de regles que hem demanat abans
+         * 2.1.2.1- ((partida grafica))
+         *
+         * 2.2.-Si es volgués carregar una partida, demanar el nom del fitxer on hi ha la partida que volem carregar.
+         *
+         */
         try {
-            if (args.length == 2) {
-                int files = Integer.parseInt(args[0]);
-                int columnes = Integer.parseInt(args[1]);
-                PartidaText.juga(new Partida(files,columnes));
+            if (args.length == 0) {
+                PartidaText.iniciaAplicacio();
                 return;
             }
-            else if (args.length == 3 && args[2].equals("-g")) {
-                Application.launch(PartidaGrafica.class, args);
+            else if (args.length == 1 && args[0].equals("-g")) {
+                System.out.println("Benvingut a l'aplicació!!");
+                Application.launch(PartidaGrafica.class);
                 return;
             }
         }
--- src/Partida/PartidaGrafica.java
+++ src/Partida/PartidaGrafica.java
@@ -1,8 +1,161 @@
 package Partida;
 
-public class PartidaGrafica {
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.beans.property.ReadOnlyDoubleProperty;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
-    public static void juga(Partida partida){
-        
+public class PartidaGrafica extends Application{
+
+    private static Scene escenaPrincipal, escenaSec;
+    private static Stage window; //aqui hi posarem el primaryStage, s'ha de fer per a l'hora de fer el canvi d'escena des d'un botó
+    private static Partida _partida;
+
+    public static void main(){
+        launch(); //crida init+start
     }
+
+    /*@Override
+    public void init(){
+        super.init();
+    }
+    */
+    @Override
+    public void start(Stage primaryStage){
+        crearEscenaPrincipal();
+        window = primaryStage;
+        window.setScene(escenaPrincipal);
+        window.setTitle("Joc Escacs");
+        window.show();
+    }
+
+    private void crearEscenaPrincipal(){
+        VBox opcions = new VBox(10);
+        BorderPane root = new BorderPane();
+        Button btnCom = new Button("Començar nova partida");
+        Button btnCar = new Button("Carregar una partida");
+        btnCom.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Es començarà una nova partida");
+                escenaSecondaria(1);
+                window.setScene(escenaSec);
+            }
+        });
+        btnCar.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Es carregarà una partida");
+                escenaSecondaria(2);
+                window.setScene(escenaSec);
+            }
+        });
+        root.setStyle("-fx-background-image: url("+ "/Images/FonsPrincipal.png" + ");-fx-background-size: stretch;");
+        opcions.setAlignment(Pos.CENTER);
+
+        opcions.getChildren().add(btnCom);
+        opcions.getChildren().add(btnCar);
+
+        root.setCenter(opcions);
+        root.setBottom(botoInferior("Exit"));
+
+        escenaPrincipal = new Scene(root, 500d, 500d);
+    }
+
+    private void escenaSecondaria(int opcio){
+        BorderPane root = new BorderPane();
+
+        if(opcio == 1){
+            root.setStyle("-fx-background-image: url("+ "/Images/fonsComencar.jpg" + ");-fx-background-size: stretch;");
+            root.setTop(topLabel("Entra el fitxer de regles que vols començar", root.widthProperty()));
+        }
+        else{
+            root.setStyle("-fx-background-image: url("+ "/Images/fonsCarregar.jpg" + ");-fx-background-size: stretch;");
+            root.setTop(topLabel("Entra el fitxer de la partida que vols carregar", root.widthProperty()));
+        }
+        root.setBottom(botoInferior("Cancelar"));
+
+        root.setCenter(prepararCentre());
+
+        escenaSec = new Scene(root, 600d,500d);
+
+    }
+
+    private Node prepararCentre() {
+        TextField nomFitxer = new TextField();
+        nomFitxer.setPromptText("Ex: nomFitxer.json");
+        Label lbl = new Label("Nom fitxer:");
+        Label missatge = new Label();
+        HBox hb = new HBox(10);
+        hb.getChildren().addAll(lbl, nomFitxer);
+        hb.setAlignment(Pos.CENTER);
+        VBox vb = new VBox(10);
+        Button subBtn = new Button("Submit");
+        Button rmvBtn = new Button("Cancelar");
+        subBtn.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                //_partida = new Partida(nomFitxer.getText());
+                String s = nomFitxer.getText();
+                if(s.isEmpty() || s == null){
+                    missatge.setText("Entre un fitxer vàlid");
+                }
+                else{
+                    System.out.println(nomFitxer.getText());
+                }
+            }
+        });
+        rmvBtn.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                nomFitxer.clear();
+            }
+        });
+
+        vb.getChildren().addAll(hb,subBtn, missatge);
+        vb.setAlignment(Pos.CENTER);
+        return vb;
+    }
+
+    private static Node topLabel(String s, ReadOnlyDoubleProperty d){
+        Label lbl = new Label(s);
+        lbl.setAlignment(Pos.BASELINE_CENTER);
+        lbl.setPrefHeight(60);
+        lbl.prefWidthProperty().bind(d);
+        lbl.setStyle("-fx-font-weight: bolder; -fx-font-size: 25px; -fx-font-family: " + "Arial Black" + "; -fx-fill: #818181; " +
+                "-fx-effect: innershadow(three-pass-box, rgb(255,99,71), 6, 0.0, 0, 2);");
+
+       return lbl;
+    }
+
+    private static Node botoInferior(String s){
+        Pane root = new Pane();
+        Button btnInf = new Button(s);
+        if(s.equals("Exit")){
+            btnInf.setOnAction(e -> Platform.exit());
+        }
+        else {
+            btnInf.setOnAction(e -> window.setScene(escenaPrincipal));
+        }
+        root.setPrefHeight(60);
+
+        btnInf.setLayoutX(30);
+        btnInf.setLayoutY(5);
+        root.getChildren().add(btnInf);
+
+        return root;
+    }
+
 }

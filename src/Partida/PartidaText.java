4fe986fe4fdaa13fadc43fb7b4860842d559a34a
MarcRogerUDG <62808807+MarcRogerUDG@users.noreply.github.com>
2020-05-24 15:51:43 +0200

    PartidaText gairabé acabat

--- src/Partida/PartidaText.java
+++ src/Partida/PartidaText.java
@@ -1,10 +1,10 @@
 package Partida;
 
-import com.sun.xml.internal.ws.wsdl.writer.document.Part;
-
 import java.util.Scanner;
 
 public abstract class PartidaText {
+
+    private static Partida _partida;
 
     public static void iniciaAplicacio() {
         Scanner teclat = new Scanner(System.in);
@@ -21,89 +21,106 @@ public abstract class PartidaText {
         if(opcio == "Començar"){System.out.println("Entra el fitxer de regles: (nomFitxer.json)"); }
         else{System.out.println("Entra el fitxer de la partida que vols carregar:");}
         String nomFitxer = teclat.nextLine();
-        Partida partida = new Partida(nomFitxer); //un metode que carrega el fitxer.
-        jugar(partida);
+        _partida = new Partida(nomFitxer); //un metode que carrega el fitxer.
+        jugar();
     }
 
-    private static void jugar(Partida partida){
+    private static void jugar(){
         boolean continuar = true;
-        Taulell taulell = partida.getTaulell();
-        String primerEquip = partida.getProperTorn();
-        boolean colorTorn = false;
-        if (primerEquip.equals("BLANQUES")){
-            colorTorn = true;
-        }
+        Scanner teclat = new Scanner(System.in);
+        String colorTorn = _partida.getProperTorn();
+        String posInici = null; //encara que es digui posInici, potser hi ha "taules" o "rendirse"
+        String posFinal = null;
+        String res = null;
         do{
-            //Declarem les posicions del moviment que vol fer el jugador, les inicialitzem a null.
-            Posicio posInici = null;
-            Posicio posFinal = null;
-            Peca pecaMoguda = null;
+            if(res.equals("taules")){
+                System.out.println("Acceptes les taules? (Si/No)");
+                boolean correcte = false;
+                do {
+                    posInici = teclat.nextLine();
+                    if(posInici.equals("Si") || posInici.equals("No")){ correcte = true; }
+                }while(!correcte);
+                if(posInici.equals("Si")){
+                    //_partida.taules();
+                    continuar = false;
+                }
+            }else{
+                posInici = llegirPosicioInici();
+                if(posInici.equals("Rendirse")){
+                    System.out.println("El jugador amb les peces " + colorTorn + " es rendeix");
+                    continuar = false;
+                    //_partida.rendirse();
+                }else if(posInici.equals("Taules")){
+                    res = "taules";
+                }else if(posInici.equals("Ajornar")){
+                    //res = _partida.ajornar();
+                }else{
+                    posInici = posInici+ " ";
+                    posFinal = llegirPosicioDesti();
+                    //res = _partida.ferTirada(posInici+posInici);
+                }
 
-            //Demanem la primera posició.
-            System.out.println("Entra la posició on es troba la peça que vols jugar");
-            pecaMoguda = llegirPosicioInici(posInici, taulell, colorTorn);
-
-            if (pecaMoguda == null) {
-                System.out.println("Alguna cosa ha sortit malament per a moure aquesta peça");
-            } else {
-                TiradaSimple tiradaActual = new TiradaSimple(posInici, posFinal, colorTorn);
-                //Demanem la segona posició.
-                System.out.println("Entra la posició on vols que vagi la peça seleccionada");
-                llegirPosicioDesti(posFinal, taulell, colorTorn, pecaMoguda, tiradaActual);
+                processarMissatge(res);
             }
-
-            //taulell.mostrarTaulellText();
-
+            if(res.equals("Tirada feta")){
+                //colorTorn = _partida.canviarTorn();
+                //_partida.mostrarTaulell();
+            }
         }while(continuar);
     }
 
-    private static Peca llegirPosicioInici(Posicio p, Taulell taulell, boolean colorTorn){
-        boolean correcte = false;
-        Peca pecaActual = null;
-        do{
-            Scanner teclat = new Scanner(System.in);
-            String pos = teclat.nextLine();
+    private static void processarMissatge(String s){
+        //mirar el que retorna "ferTirada"
+        //Podria ser que el moviment fos invalid ...................
 
-            p = new Posicio(pos);
-            if (taulell.contePeçaCasella(p)) {
-                pecaActual = taulell.getPeca(p);
-                if (pecaActual.get_equip() == colorTorn) {
-                    correcte = true;
-                }
-                else {
-                    System.out.println("Peça del color incorrecte");
-                }
-            }
-            else {
-                System.out.println("Posició invàlida, entre'n una altre");
-            }
-        } while(!correcte);
-        return pecaActual;
     }
 
-    private static void llegirPosicioDesti(Posicio p, Taulell taulell, boolean colorTorn, Peca pecaMoguda, TiradaSimple tiradaActual){
+    private static String llegirPosicioInici(){
         boolean correcte = false;
+        String s;
+        Scanner teclat = new Scanner(System.in);
         do{
-            Scanner teclat = new Scanner(System.in);
-            String pos = teclat.nextLine();
-
-            p = new Posicio(pos);
-            if (taulell.contePeçaCasella(p)) {
-                Peca pecaActual = taulell.getPeca(p);
-                if (pecaActual.get_equip() != colorTorn && pecaMoguda.movimentValid(tiradaActual)) {
-                    correcte = true;
-                } else if (pecaActual.get_equip() == colorTorn && pecaMoguda.movimentValid(tiradaActual)) {
-                    System.out.println("Posició ja ocupada per una peça del teu equip");
-                } else if (!pecaMoguda.movimentValid(tiradaActual)) {
-                    System.out.println("Moviment invàlid");
-                } else {
-                    System.out.println("No pots realitzar aquest moviment");
-                }
-            } else if (!taulell.contePeçaCasella(p) && pecaMoguda.movimentValid(tiradaActual)){
-                correcte = true;
-            } else {
-                System.out.println("No pots realitzar aquest moviment");
+            s = teclat.nextLine();
+            if(s.equals("Rendirse") || s.equals("Taules") || s.equals("Ajornar")){
+                return s;
+            }
+            else{
+                //si entra aqui vol dir que ha entrat una posicio o aixo creiem
+                correcte = posicioCorrecte(s);
             }
         } while(!correcte);
+        return s;
+    }
+
+    private static String llegirPosicioDesti(){
+        boolean correcte = false;
+        String s = null;
+        String res = "";
+        Scanner teclat = new Scanner(System.in);
+        do{
+            if(s.equals("-")){ //vol dir que vol fer enroc
+                res = "- ";
+            }else if(s.equals("  ")){ //ha entrat una posició
+                correcte = posicioCorrecte(s);
+                res = res + s;
+            }
+        } while(!correcte);
+        return res;
+    }
+
+    private static boolean posicioCorrecte(String s){
+        boolean correcte = false;
+        /*String res = _partida.posCorrecte(s);
+        if(res.equals("NO")){ //no es una posicio
+
+        }else if(res.equals("Posició invàlida")) { //posicio sense peça
+
+        }else if(res.equals("Posició amb una peça")){ //la peça no es del teu color
+
+        }
+        else{
+            correcte = true;
+        }*/
+        return correcte;
     }
 }

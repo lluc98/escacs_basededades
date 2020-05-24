dd1e21fee75b671c0ad9f85694176d7ce9da6b11
Sergi Torrent <sergi.torrent.fulla@gmail.com>
2020-05-24 17:54:53 +0200

    [MODIFIED] Partida, fer tirada interactua amb jugador, historial gurada partida i inicia nova

--- src/Partida/Historial.java
+++ src/Partida/Historial.java
@@ -15,7 +15,6 @@ public class Historial {
     public static void iniciarPartidaNova() {
         String resultatfinal;
 
-        FileWriter fitxerPartida;
         JSONObject partida = new JSONObject(adrecaFitxer);
         //Hauriem de comprovar si ja existeix el fitxer
         partida.put("fitxerRegles", "");
@@ -26,22 +25,30 @@ public class Historial {
         JSONArray posIniNegres = new JSONArray();
         partida.put("posIniBlanques", posIniNegres);
 
-        partida.put("proper_torn","");
+        partida.put("proper_torn", "");
 
         JSONArray tirades = new JSONArray();
         partida.put("tirades", tirades);
 
-        partida.put("resultat_final","");
+        partida.put("resultat_final", "");
+
+        try {
+            FileWriter fitxerPartida = new FileWriter("PartidaNova.txt");
+            fitxerPartida.write(partida.toString());
+            fitxerPartida.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    public static void guardarTirada(String[] args) {
+        public static void guardarTirada(String[] args) {
         try {
             JSONObject infoTirada = new JSONObject();
             infoTirada.put("torn", args[0]);
             infoTirada.put("origen", args[1]);
             infoTirada.put("desti", args[2]);
             infoTirada.put("resultat", args[3]);
-            String contingut = new String((Files.readAllBytes((Paths.get("PartidaActual.txt")))));
+            String contingut = new String((Files.readAllBytes((Paths.get(adrecaFitxer)))));
             JSONObject fitxerTirades = new JSONObject(contingut);
             JSONArray tirades = fitxerTirades.getJSONArray("tirades");
             tirades.put(infoTirada);
@@ -52,7 +59,7 @@ public class Historial {
 
     public static void getUltimaTirada(String torn, String origen, String desti, String resultat) {
         try {
-            String contingut = new String((Files.readAllBytes((Paths.get("PartidaActual.txt")))));
+            String contingut = new String((Files.readAllBytes((Paths.get(adrecaFitxer)))));
             JSONObject fitxerTirades = new JSONObject(contingut);
             JSONArray tirades = fitxerTirades.getJSONArray("tirades");
             JSONObject tirada = tirades.getJSONObject(tirades.length() - 1);
@@ -68,7 +75,7 @@ public class Historial {
 
     public static void eliminarUltimaTirada() {
         try {
-            String contingut = new String((Files.readAllBytes((Paths.get("PartidaActual.txt")))));
+            String contingut = new String((Files.readAllBytes((Paths.get(adrecaFitxer)))));
             JSONObject fitxerTirades = new JSONObject(contingut);
             JSONArray tirades = fitxerTirades.getJSONArray("tirades");
             tirades.remove(tirades.length() - 1);
@@ -77,7 +84,24 @@ public class Historial {
         }
     }
 
-    public static void guardarPartida () {
-
+    public static void guardarPartida(String resultat) {
+        //resultat = "BLANQUES", han guanyat les blanques, "NEGRES" viceversa
+        //resultat = "TAULES"
+        //resultat = "AJORNAT"
+        try {
+            String contingut = new String((Files.readAllBytes((Paths.get(adrecaFitxer)))));
+            JSONObject fitxerPartida = new JSONObject(contingut);
+            if (resultat == "BLANQUES") {
+                fitxerPartida.put("resultat_final", "BLANQUES GUANYEN");
+            } else if (resultat == "NEGRES") {
+                fitxerPartida.put("resultat_final", "NEGRES GUANYEN");
+            } else if (resultat == "TAULES") {
+                fitxerPartida.put("resultat_final", "TAULES");
+            } else {
+                fitxerPartida.put("resultat_final", "");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
